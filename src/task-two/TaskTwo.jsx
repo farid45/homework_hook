74ceb90882f2c76ff6a89c12d676fafdc1c7680c
fariddzenetov <fariddjenetov75@gmail.com>
2025-07-18 00:44:00 +0300

    2

--- src/task-two/TaskTwo.jsx
+++ src/task-two/TaskTwo.jsx
@@ -37,7 +37,7 @@ const Input = ({ onChange }) => {
     )
 }
 
-// Мемоизированные версии компонентов
+// Мемоизированние компонентов
 const MemoizedRoot = memo(Root);
 const MemoizedInput = memo(Input);
 
